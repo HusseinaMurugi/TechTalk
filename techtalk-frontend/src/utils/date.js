2e4248d317ea9102c80d639059fe3cc92844af86
HusseinaMurugi <hussxinamurugi@gmail.com>
2026-01-17 11:27:11 +0300

    deleted all files, repo is empty

--- techtalk-frontend/src/utils/date.js
+++
@@ -1,6 +0,0 @@
-// Format timestamp to human-readable format
-import { formatDistanceToNow } from 'date-fns';
-
-export const formatDate = (timestamp) => {
-  return formatDistanceToNow(new Date(timestamp), { addSuffix: true });
-};
